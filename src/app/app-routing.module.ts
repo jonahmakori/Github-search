d3fe6a4c07e4ac2421ac5a007826ce2fb3c3eabb
jonahmakori <jonahmakori0@gmail.com>
2020-01-23 21:43:46 +0300

    imported the clasess in appmodule and approuting ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:"users",component:UsersComponent},
+    {path:"repos",component:ReposComponent},
+    {path:"",redirectTo:"/users",pathMatch:"full"},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './users/users.component';
+import { ReposComponent } from './repos/repos.component';
+import { DataService } from './services/data.service';
+import { HttpModule } from '@angular/http';
+import { LastPipe } from './pipe/last.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ReposComponent,
+    LastPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
